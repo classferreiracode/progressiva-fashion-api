edaafbc7aee9b92834b9e4e4f767dcec76480b91
Wagner Ferreira <ferreirasi.tk@outlook.com>
2025-06-16 17:27:30 -0300

    refactor: Remove ordering from product index query

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -17,7 +17,7 @@ class ProductController extends Controller
             $query->whereHas('category', fn($q) => $q->where('slug', $request->category));
         }
 
-        $produtos = $query->orderBy('created_at', 'desc')->get();
+        $produtos = $query->get();
 
         return response()->json($produtos);
     }
